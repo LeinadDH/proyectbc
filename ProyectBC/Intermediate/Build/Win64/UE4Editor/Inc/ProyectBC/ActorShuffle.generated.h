a078c7c4fb882fbcd8b07a4343f718716e1470bb
SrAlmeja <cdmx1895@amerike.edu.mx>
2023-09-19 20:37:46 -0600

    rotisimo

---
+++ ProyectBC/Intermediate/Build/Win64/UE4Editor/Inc/ProyectBC/ActorShuffle.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PROYECTBC_ActorShuffle_generated_h
+#error "ActorShuffle.generated.h already included, missing '#pragma once' in ActorShuffle.h"
+#endif
+#define PROYECTBC_ActorShuffle_generated_h
+
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_SPARSE_DATA
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_RPC_WRAPPERS
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAActorShuffle(); \
+	friend struct Z_Construct_UClass_AActorShuffle_Statics; \
+public: \
+	DECLARE_CLASS(AActorShuffle, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ProyectBC"), NO_API) \
+	DECLARE_SERIALIZER(AActorShuffle)
+
+
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAActorShuffle(); \
+	friend struct Z_Construct_UClass_AActorShuffle_Statics; \
+public: \
+	DECLARE_CLASS(AActorShuffle, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ProyectBC"), NO_API) \
+	DECLARE_SERIALIZER(AActorShuffle)
+
+
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AActorShuffle(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AActorShuffle) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AActorShuffle); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AActorShuffle); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AActorShuffle(AActorShuffle&&); \
+	NO_API AActorShuffle(const AActorShuffle&); \
+public:
+
+
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AActorShuffle(AActorShuffle&&); \
+	NO_API AActorShuffle(const AActorShuffle&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AActorShuffle); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AActorShuffle); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AActorShuffle)
+
+
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_PRIVATE_PROPERTY_OFFSET
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_9_PROLOG
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_PRIVATE_PROPERTY_OFFSET \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_SPARSE_DATA \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_RPC_WRAPPERS \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_INCLASS \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ProyectBC_Source_ProyectBC_ActorShuffle_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_PRIVATE_PROPERTY_OFFSET \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_SPARSE_DATA \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_INCLASS_NO_PURE_DECLS \
+	ProyectBC_Source_ProyectBC_ActorShuffle_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PROYECTBC_API UClass* StaticClass<class AActorShuffle>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ProyectBC_Source_ProyectBC_ActorShuffle_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
